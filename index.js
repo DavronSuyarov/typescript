5c36399b7147c379fcc4524d88cf8c24f6d61807
Davron <shamol1302@gmail.com>
2025-09-20 10:08:45 +0500

    Typoripit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Salom duyo');
+console.log(false + 34 + true);
+console.log("TypeScrpit Qo'shildi");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,3 @@
+console.log('Salom duyo');
+console.log(false + 34 + true);
+console.log("TypeScrpit Qo'shildi");
