4543d14d49c3ec51984d7f07dafdfdf655c652d6
Davron <shamol1302@gmail.com>
2025-09-23 18:19:59 +0500

    davomi

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+"use strict";
 // function pow(x: number, y: number): number {
 // 	return x ** y;
 // }
 function pow(x, y) {
-    return "".concat(x, "**").concat(y, "=").concat(Math.pow(x, y));
+    return `${x}**${y}=${x ** y}`;
 }
-console.log(pow(4, 2));
-var add = function (x, y) { return x + y; };
+// console.log(pow(4, 2));
+const add = (x, y) => x + y;
+function someFunc(s) {
+    throw new Error();
+}
+// signature function
+let c;
+function overloadFunc(x, y) {
+    if (typeof x === 'number' && typeof y === 'number') {
+        return x + y;
+    }
+    else {
+        return `${x} ${y}`;
+    }
+}
+console.log(overloadFunc('Answer', 4));
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -12,3 +12,25 @@ const add = (x: number, y: number): number => x + y;
 function someFunc(s: string): never {
 	throw new Error();
 }
+
+// signature function
+let c: (x: number, y: string) => string;
+// c = function (a: number, b: string): number {
+// 	return a + b;
+// };
+
+// c = function (a: number, b: string): string {
+// 	return `${b}:${a}`;
+// };
+// console.log(c(3, 'Javob'));
+function overloadFunc(x: number, y: number): number;
+function overloadFunc(x: string, y: number): string;
+function overloadFunc(x: any, y: any): any {
+	if (typeof x === 'number' && typeof y === 'number') {
+		return x + y;
+	} else {
+		return `${x} ${y}`;
+	}
+}
+
+console.log(overloadFunc('Answer', 4));
