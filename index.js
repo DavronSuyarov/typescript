03e3d33dd4864e20a6a5c21747719814cc0c82c1
Davron <shamol1302@gmail.com>
2025-09-24 22:41:57 +0500

    wednesday plan

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 "use strict";
-// function pow(x: number, y: number): number {
-// 	return x ** y;
+// // function pow(x: number, y: number): number {
+// // 	return x ** y;
+// // }
+// function pow(x: number, y: number): string {
+// 	return `${x}**${y}=${x ** y}`;
 // }
-function pow(x, y) {
-    return `${x}**${y}=${x ** y}`;
+let obj;
+obj = { name: 'Davron' };
+obj = { age: 39 };
+obj = { age: 39, name: 'Suyarov' };
+let obj2;
+obj2 = { name: 'Sayyod', age: 30 };
+let obj3;
+// obj3 = { name: 'Fotima', age: 37 };
+obj3 = { name: 'Soit' };
+if ('age' in obj3) {
+    console.log('Bor');
 }
-// console.log(pow(4, 2));
-const add = (x, y) => x + y;
-function someFunc(s) {
-    throw new Error();
+else {
+    console.log('Yoq');
 }
-// signature function
-let c;
-function overloadFunc(x, y) {
-    if (typeof x === 'number' && typeof y === 'number') {
-        return x + y;
-    }
-    else {
-        return `${x} ${y}`;
-    }
-}
-console.log(overloadFunc('Answer', 4));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,36 +1,60 @@
-// function pow(x: number, y: number): number {
-// 	return x ** y;
+// // function pow(x: number, y: number): number {
+// // 	return x ** y;
+// // }
+// function pow(x: number, y: number): string {
+// 	return `${x}**${y}=${x ** y}`;
 // }
-function pow(x: number, y: number): string {
-	return `${x}**${y}=${x ** y}`;
+
+// // console.log(pow(4, 2));
+
+// const add = (x: number, y: number): number => x + y;
+
+// function someFunc(s: string): never {
+// 	throw new Error();
+// }
+
+// // signature function
+// let c: (x: number, y: string) => string;
+// // c = function (a: number, b: string): number {
+// // 	return a + b;
+// // };
+
+// // c = function (a: number, b: string): string {
+// // 	return `${b}:${a}`;
+// // };
+// // console.log(c(3, 'Javob'));
+// function overloadFunc(x: number, y: number): number;
+// function overloadFunc(x: string, y: number): string;
+// function overloadFunc(x: any, y: any): any {
+// 	if (typeof x === 'number' && typeof y === 'number') {
+// 		return x + y;
+// 	} else {
+// 		return `${x} ${y}`;
+// 	}
+// }
+
+// // console.log(overloadFunc('Answer', 4));
+
+type OBJ = { name: string } | { age: number };
+
+let obj: OBJ;
+obj = { name: 'Davron' };
+obj = { age: 39 };
+obj = { age: 39, name: 'Suyarov' };
+
+type OBJ2 = { name: string } & { age: number };
+let obj2: OBJ2;
+obj2 = { name: 'Sayyod', age: 30 };
+
+type OBJ3 = { name?: string; age?: number };
+
+let obj3: OBJ3;
+
+// obj3 = { name: 'Fotima', age: 37 };
+obj3 = { name: 'Soit' };
+
+if ('age' in obj3) {
+	console.log('Bor');
+} else {
+	console.log('Yoq');
 }
-
-// console.log(pow(4, 2));
-
-const add = (x: number, y: number): number => x + y;
-
-function someFunc(s: string): never {
-	throw new Error();
-}
-
-// signature function
-let c: (x: number, y: string) => string;
-// c = function (a: number, b: string): number {
-// 	return a + b;
-// };
-
-// c = function (a: number, b: string): string {
-// 	return `${b}:${a}`;
-// };
-// console.log(c(3, 'Javob'));
-function overloadFunc(x: number, y: number): number;
-function overloadFunc(x: string, y: number): string;
-function overloadFunc(x: any, y: any): any {
-	if (typeof x === 'number' && typeof y === 'number') {
-		return x + y;
-	} else {
-		return `${x} ${y}`;
-	}
-}
-
-console.log(overloadFunc('Answer', 4));
