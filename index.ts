e2839f454a0cc1cd9a6265315ff67753e2a40120
Davron <shamol1302@gmail.com>
2025-09-27 22:18:04 +0500

    strong

--- index.ts
+++ index.ts
@@ -35,26 +35,45 @@
 
 // // console.log(overloadFunc('Answer', 4));
 
-type OBJ = { name: string } | { age: number };
+// type OBJ = { name: string } | { age: number };
 
-let obj: OBJ;
-obj = { name: 'Davron' };
-obj = { age: 39 };
-obj = { age: 39, name: 'Suyarov' };
+// let obj: OBJ;
+// obj = { name: 'Davron' };
+// obj = { age: 39 };
+// obj = { age: 39, name: 'Suyarov' };
 
-type OBJ2 = { name: string } & { age: number };
-let obj2: OBJ2;
-obj2 = { name: 'Sayyod', age: 30 };
+// type OBJ2 = { name: string } & { age: number };
+// let obj2: OBJ2;
+// obj2 = { name: 'Sayyod', age: 30 };
 
-type OBJ3 = { name?: string; age?: number };
+// type OBJ3 = { name?: string; age?: number };
 
-let obj3: OBJ3;
+// let obj3: OBJ3;
 
-// obj3 = { name: 'Fotima', age: 37 };
-obj3 = { name: 'Soit' };
+// // obj3 = { name: 'Fotima', age: 37 };
+// obj3 = { name: 'Soit' };
 
-if ('age' in obj3) {
-	console.log('Bor');
-} else {
-	console.log('Yoq');
+// if ('age' in obj3) {
+// 	console.log('Bor');
+// } else {
+// 	console.log('Yoq');
+// }
+// ------------------------------//  Arrays Tuples & enum //--------------------------
+
+let a = [];
+// a=["sdsfsd", 23, {"djfhkjdf"}, true, undefined, null]
+
+const d: number[] = [2, 2, 3, 4];
+
+const f: Array<number> = [45, 67];
+
+let g: [number, string];
+
+g = [12, 'dsbfd'];
+
+enum Gender {
+	Male,
+	Female = 'Fotima',
 }
+
+console.log(Gender.Female);
